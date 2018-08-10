2619fa29fb9559489cb2e16c05d5124ef957f49b
vivekvgs <vivek.r@mookambikainfo.com>
2018-08-10 20:08:35 +0530

    new changes

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -12,7 +12,7 @@ class Users extends CI_Controller {
         $this->load->library('form_validation');
         if ($this->session->userdata('logged_in') == False) {
             redirect(base_url() . 'login/', 'refresh');
-        }         
+        }
     }
 
     public function index() {
@@ -26,7 +26,6 @@ class Users extends CI_Controller {
         $this->load->view('users/list', $data);
         $this->load->view('includes/footer');
     }
-    
 
     public function add($id = NULL) {
         if ($this->session->userdata('role') == 2) {
@@ -64,7 +63,7 @@ class Users extends CI_Controller {
                 $data = array('firstname' => trim($this->input->post('firstname')),
                     'lastname' => trim($this->input->post('lastname')),
                     'gender' => trim($this->input->post('gender')),
-                    'username' => trim($this->input->post('username')),                    
+                    'username' => trim($this->input->post('username')),
                     'address' => trim($this->input->post('address')),
                     'mobileno' => trim($this->input->post('mobileno')),
                     'role' => 2
@@ -156,7 +155,7 @@ class Users extends CI_Controller {
         $msg = array("image_message" => $message, "image_file_name" => $file_name);
         return $msg;
     }
-    
+
     public function staffbalance() {
         $balance_lists = $this->users_model->balancelists();
         $data = array('balance_lists' => $balance_lists);
@@ -165,7 +164,7 @@ class Users extends CI_Controller {
         $this->load->view('users/balancelist', $data);
         $this->load->view('includes/footer');
     }
-    
+
     public function staffbalanceadd($id = NULL) {
         $balance_list = array();
         if ($id != "") {
@@ -176,7 +175,7 @@ class Users extends CI_Controller {
         }
         $users_list = $this->users_model->lists();
         //echo "<pre>".print_r($balance_list);die;
-        $data = array('balance_list' => $balance_list, 'id' => $id,'users_list'=>$users_list);
+        $data = array('balance_list' => $balance_list, 'id' => $id, 'users_list' => $users_list);
         $this->load->view('includes/header');
         $this->load->view('includes/sidebar');
         $this->load->view('users/balance', $data);
@@ -196,12 +195,12 @@ class Users extends CI_Controller {
                 $data = array('amount' => trim($this->input->post('amount')),
                     'user_id' => trim($this->input->post('user_id')),
                     'buydate' => trim($this->input->post('buydate'))
-                );                
-                if ($id != "") {      
-                     $data['updated_on'] = date('Y-m-d H:i:s');
-                    $saveproduct = $this->users_model->updatebalance($data, $id);                    
+                );
+                if ($id != "") {
+                    $data['updated_on'] = date('Y-m-d H:i:s');
+                    $saveproduct = $this->users_model->updatebalance($data, $id);
                 } else {
-                     $data['created_on'] = date('Y-m-d H:i:s');
+                    $data['created_on'] = date('Y-m-d H:i:s');
                     $saveproduct = $this->users_model->savebalance($data);
                 }
                 if ($saveproduct == 1) {
@@ -213,8 +212,8 @@ class Users extends CI_Controller {
             }
         }
     }
-    
-     public function staffbalancedelete($id = NULL) {
+
+    public function staffbalancedelete($id = NULL) {
         if ($id != "") {
             $deleteUsers = $this->users_model->updatebalance(array('dels' => 1), $id);
             if ($deleteUsers == "1") {
@@ -227,4 +226,76 @@ class Users extends CI_Controller {
             redirect(base_url() . 'users/staffbalance', 'refresh');
         }
     }
+
+    public function profile($id = NULL) {
+        $users_list = array();
+        if ($this->session->userdata('id')) {
+            $users_list = $this->users_model->lists(md5($this->session->userdata('id')));
+            if (count($users_list) == 0) {
+                redirect(base_url() . 'companyorders', 'refresh');
+            }
+        }
+        $data = array('users_list' => $users_list, 'id' => $id);
+        $this->load->view('includes/header');
+        $this->load->view('includes/sidebar');
+        $this->load->view('users/profile', $data);
+        $this->load->view('includes/footer');
+    }
+
+    public function ajaxsaveprofile() {
+
+
+        if (($this->input->server('REQUEST_METHOD') == 'POST')) {
+            $this->form_validation->set_rules('firstname', 'First Name', 'trim|required|min_length[3]|max_length[150]');
+            $this->form_validation->set_rules('lastname', 'Last Name', 'trim|required|min_length[3]|max_length[150]');
+            $this->form_validation->set_rules('gender', 'Email', 'trim|required');
+            $this->form_validation->set_rules('address', 'Address', 'trim|required|min_length[3]|max_length[250]');
+
+            if ($this->form_validation->run() == FALSE) {
+                echo json_encode(array('status' => 0, 'msg' => validation_errors()));
+                return false;
+            } else {
+                $data = array('firstname' => trim($this->input->post('firstname')),
+                    'lastname' => trim($this->input->post('lastname')),
+                    'gender' => trim($this->input->post('gender')),
+                    'address' => trim($this->input->post('address'))
+                );
+                if ($this->session->userdata('id')) {
+                    $users_list = $this->users_model->lists(md5($this->session->userdata('id')));
+                }
+                if (isset($_FILES['userimage']['name']) && (!empty($_FILES['userimage']['name']))) {
+                    $upload_image = $this->do_upload_image('userimage');
+                    if ($upload_image['image_message'] == "success") {
+
+                        //Remove product Image
+                        if (isset($users_list[0]['userimage']) && !empty($users_list[0]['userimage'])) {
+                            $image_file = './upload/users/' . $users_list[0]['userimage'];
+                            if (file_exists($image_file)) {
+                                unlink($image_file);
+                            }
+                        }
+
+                        $data['userimage'] = trim($upload_image['image_file_name']);
+                    } else {
+                        echo json_encode(array('status' => 0, 'msg' => "<p>Please upload only image</p>"));
+                        return false;
+                    }
+                }
+
+                if ($this->session->userdata('id')) {
+                    $data['updated_on'] = date('Y-m-d H:i:s');
+                    $saveusers = $this->users_model->update($data, md5($this->session->userdata('id')));
+                }
+                if ($saveusers == 1) {
+                    $this->session->set_userdata('firstname', $this->input->post('firstname'));
+                    $this->session->set_userdata('lastname', $this->input->post('lastname'));                    
+                    $this->session->set_flashdata('SucMessage', 'Profile Saved Successfully');
+                    echo json_encode(array('status' => 1));
+                } else {
+                    echo json_encode(array('status' => 0, 'msg' => 'Profile Saved Not Successfully'));
+                }
+            }
+        }
+    }
+
 }
---
+++ application/views/users/profile.php
@@ -0,0 +1,228 @@
+<section class="content">
+    <div class="container-fluid">
+        <div class="block-header">
+            <h2>
+                Profile                    
+            </h2>
+        </div>
+        <!-- Basic Validation -->
+        <div class="row clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="card">
+                    <div class="header">
+                        <h2>Profile Details</h2>                        
+                    </div>
+                    <div class="body">
+                        <div class="alert bg-red" style="display:none;">
+
+                        </div>
+                        <?php if ($this->session->flashdata('SucMessage') != '') {
+                            ?>
+                            <div class="alert bg-green alert-dismissible" role="alert">
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+                                <?php echo $this->session->flashdata('SucMessage'); ?>
+                            </div>                            
+                        <?php } ?>
+                        <form id="userform" method="POST" name="userform" action="<?php echo base_url() . 'users/ajaxsaveprofile/'; ?>" enctype="multipart/form-data">
+                            <div class="form-group form-float">
+                                <div class="form-line">
+                                    <input type="text" class="form-control" name="firstname" id="firstname" value="<?php echo isset($users_list[0]['firstname']) ? $users_list[0]['firstname'] : ''; ?>" required>
+                                    <label class="form-label">First Name</label>
+                                </div>
+                            </div>                           
+                            <div class="form-group form-float">
+                                <div class="form-line">
+                                    <input type="text" class="form-control" name="lastname" id="lastname" value="<?php echo isset($users_list[0]['lastname']) ? $users_list[0]['lastname'] : ''; ?>" required>
+                                    <label class="form-label">Last Name</label>
+                                </div>
+                            </div>                           
+                            <div class="form-group form-float">
+                                <div class="form-line">
+                                    <textarea name="address" id="address" cols="30" rows="5" class="form-control no-resize" required><?php echo isset($users_list[0]['address']) ? $users_list[0]['address'] : ''; ?></textarea>
+                                    <label class="form-label">Address</label>
+                                </div>
+                            </div>
+                            <div class="form-group form-float">
+                                <div class="form-line">
+                                    <input type="text" class="form-control" name="mobileno" id="mobileno" maxlength="10" value="<?php echo isset($users_list[0]['mobileno']) ? $users_list[0]['mobileno'] : ''; ?>" required>
+                                    <label class="form-label">Mobile No</label>
+                                </div>
+                            </div>                           
+                            <div class="form-group form-float">
+                                <div class="form-line">
+                                    <input type="text" class="form-control" name="username" id="username" value="<?php echo isset($users_list[0]['username']) ? $users_list[0]['username'] : ''; ?>" disabled="">
+                                    <label class="form-label">User Name</label>
+                                </div>
+                            </div>                  
+                            <div class="form-group form-float">
+                                <label class="form-label">Gender</label>
+                                <div class="form-line">
+                                    <input name="gender" type="radio" id="radio_1" <?php echo (isset($users_list[0]['gender']) && $users_list[0]['gender'] == "male") ? "checked" : ''; ?> value="male" required=""/>
+                                    <label for="radio_1">Male</label>
+                                    <input name="gender" type="radio" id="radio_2" <?php echo (isset($users_list[0]['gender']) && $users_list[0]['gender'] == "female") ? "checked" : ''; ?> value="female" required=""/>
+                                    <label for="radio_2">Female</label>
+
+                                </div>
+                            </div>                  
+                            <div class="form-group form-float">
+
+                                <?php
+                                $display = "";
+                                if (isset($users_list[0]['userimage']) && !empty($users_list[0]['userimage'])) {
+                                    $display = "none";
+                                    if (file_exists("./upload/users/" . $users_list[0]['userimage'])) {
+                                        $image_name = $users_list[0]['userimage'];
+                                    } else {
+                                        $image_name = "no_image.png";
+                                    }
+                                    ?>
+
+                                    <div id='usershowimage'> 
+                                        <img class="img-thumbnail" src="<?php echo base_url() . 'upload/users/' . $image_name; ?>" alt="" width="100" height="100"/>
+                                        &nbsp;&nbsp;<a href="javascript:void(0);" onclick="RemoveImage();" class="btn bg-brown waves-effect" title="Delete Product">Remove</a>
+                                    </div>
+
+                                <?php } ?>
+                                <div class="form-line">
+                                    <div id="userimagecontent" style="padding-left: 5px;display:<?php echo $display; ?>;">
+                                        <input type="file" name="userimage" id="userimage" accept="image/png, image/jpeg, image/gif" name="input-file-preview"/> <!-- rename it -->
+                                    </div>
+                                </div>
+
+                            </div>
+
+
+                            <a href="<?php echo base_url(); ?>users" class="btn bg-blue-grey waves-effect" onclick="return confirm('Are you sure cancel the data?')">Cancel</a>
+                            <button class="btn btn-primary waves-effect" type="submit">SUBMIT</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- #END# Basic Validation -->        
+    </div>
+</section>
+
+<script src="<?php echo base_url() . 'assets/plugins/jquery-validation/jquery.validate.js'; ?>"></script>
+
+<script type="text/javascript">
+
+                                function RemoveImage()
+                                {
+                                    $("#usershowimage").hide();
+                                    $("#userimagecontent").show();
+                                }
+                                $(function () {
+
+                                    $('#userform').validate({
+                                        highlight: function (input) {
+                                            $(input).parents('.form-line').addClass('error');
+                                        },
+                                        unhighlight: function (input) {
+                                            $(input).parents('.form-line').removeClass('error');
+                                        },
+                                        errorPlacement: function (error, element) {
+                                            $(element).parents('.form-group').append(error);
+                                        },
+                                        rules: {
+                                            firstname: {
+                                                required: true,
+                                                minlength: 3,
+                                                maxlength: 150
+                                            },
+                                            lastname: {
+                                                required: true,
+                                                minlength: 3,
+                                                maxlength: 150
+                                            },
+                                            address: {
+                                                required: true,
+                                                maxlength: 250,
+                                            },
+                                            gender: {
+                                                required: true
+                                            },
+                                            mobileno: {
+                                                required: true,
+                                                digits: true,
+                                                minlength: 10,
+                                                maxlength: 10
+                                            }
+                                        },
+                                        messages: {
+                                            firstname: {
+                                                required: "Please enter first name"
+
+                                            },
+                                            lastname: {
+                                                required: "Please enter last name"
+
+                                            },
+                                            address: {
+                                                required: "Please enter the address"
+
+                                            },
+                                            gender: {
+                                                required: "Please choose the gender"
+
+                                            },
+                                            mobileno: {
+                                                required: "Please enter mobile number"
+
+                                            }
+                                        },
+                                        submitHandler: function (form) {
+                                            var formData = new FormData($('#userform')[0]);
+                                            formData.append('userimage', $('input[type=file]')[0].files[0]);
+                                            var $form = $("#userform");
+                                            $.ajax({
+                                                type: $form.attr('method'),
+                                                url: $form.attr('action'),
+                                                data: formData,
+                                                async: false,
+                                                cache: false,
+                                                contentType: false,
+                                                processData: false,
+                                                dataType: 'json'
+                                            }).done(function (response) {
+
+                                                if (response.status == "1")
+                                                {
+                                                    window.location = "<?php echo base_url() . 'users/profile'; ?>";
+                                                } else
+                                                {
+                                                    $('.bg-red').show();
+                                                    $('.bg-red').html(response.msg);
+                                                    setTimeout(function () {
+                                                        $('.bg-red').hide('slow');
+                                                    }, 4000);
+                                                }
+                                            });
+                                            return false; // required to block normal submit since you used ajax
+                                        }
+                                    });
+                                    $.validator.addMethod("existusername", function (value, element) {
+                                        var checkUser = check_exist_user(value);
+                                        if (checkUser == "1")
+                                        {
+                                            return false;
+                                        }
+                                        return true;
+
+                                    }, "User Name Already Exists!");
+                                });
+                                function check_exist_user(username) {
+                                    var isSuccess = 0;
+                                    $.ajax({
+                                        type: "POST",
+                                        url: "<?php echo base_url(); ?>users/exist_users_check",
+                                        data: "username=" + username + "&id=" + "<?php echo $id; ?>",
+                                        async: false,
+                                        success:
+                                                function (msg) {
+                                                    isSuccess = msg === "1" ? 1 : 0
+                                                }
+                                    });
+                                    return isSuccess;
+                                }
+</script>
